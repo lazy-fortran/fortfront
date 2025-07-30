ed0842807b837dcfb228ed712ddeaf21a4e79e90
Christopher Albert <albert@tugraz.at>
2025-07-30 20:19:01 +0200

    test: add Windows diagnostic test
    - Simple test to verify FPM test execution works on Windows
    - Will help identify if issue is test-specific or build-related

---
+++ test/test_windows_diagnostic.f90
@@ -0,0 +1,11 @@
+program test_windows_diagnostic
+    implicit none
+    
+    print *, "=== Windows Diagnostic Test ==="
+    print *, "This test checks if basic FPM test execution works on Windows"
+    print *, "If this test passes but others fail, the issue is test-specific"
+    print *, ""
+    print *, "Test PASSED"
+    
+    stop 0
+end program test_windows_diagnostic
