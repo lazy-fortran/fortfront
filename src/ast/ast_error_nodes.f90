55d5ef62fd1f401d10858dd5011e19b2fc3bd0f8
Christopher Albert <albert@tugraz.at>
2025-08-28 11:56:20 +0200

    fix: implement proper abstract interfaces for error_node_t
    - Remove duplicate line/column fields that exist in parent ast_node
    - Implement deferred accept procedure with correct ast_visitor_base_t signature
    - Convert to_json from function to subroutine matching to_json_interface
    - Add proper JSON module imports and JSON core API calls
    - Fix abstract type compliance for error node implementations
    
    This resolves compilation errors in ast_error_nodes.f90 module.

--- src/ast/ast_error_nodes.f90
+++ src/ast/ast_error_nodes.f90
@@ -1,5 +1,6 @@
 module ast_error_nodes
-    use ast_base, only: ast_node
+    use json_module
+    use ast_base, only: ast_node, ast_visitor_base_t
     implicit none
     private
     
@@ -10,11 +11,9 @@ module ast_error_nodes
         character(len=:), allocatable :: error_message
         character(len=:), allocatable :: original_data
         integer :: error_code = -1
-        integer :: line = 0
-        integer :: column = 0
     contains
         procedure :: to_json => error_node_to_json
-        procedure :: accept_visitor => error_node_accept_visitor
+        procedure :: accept => error_node_accept
         procedure :: cleanup => error_node_cleanup
     end type
     
@@ -35,30 +34,29 @@ contains
         end if
         node%error_code = -1
         
+        ! Set location in inherited fields
         if (present(line)) node%line = line
         if (present(column)) node%column = column
     end function
 
-    ! JSON representation for error nodes
-    function error_node_to_json(this) result(json_str)
+    ! JSON representation for error nodes (implements to_json_interface)
+    subroutine error_node_to_json(this, json, parent)
         class(error_node_t), intent(in) :: this
-        character(len=:), allocatable :: json_str
-        character(len=32) :: line_str, col_str
+        type(json_core), intent(inout) :: json
+        type(json_value), pointer, intent(in) :: parent
         
-        write(line_str, '(I0)') this%line
-        write(col_str, '(I0)') this%column
-        
-        json_str = '{"type": "error_node"' // &
-                   ', "error": "' // this%error_message // '"' // &
-                   ', "data": "' // this%original_data // '"' // &
-                   ', "line": ' // trim(line_str) // &
-                   ', "column": ' // trim(col_str) // '}'
-    end function
+        call json%add(parent, 'type', 'error_node')
+        call json%add(parent, 'error', this%error_message)
+        call json%add(parent, 'data', this%original_data)
+        call json%add(parent, 'error_code', this%error_code)
+        call json%add(parent, 'line', this%line)
+        call json%add(parent, 'column', this%column)
+    end subroutine
 
-    ! Visitor pattern support for error nodes
-    subroutine error_node_accept_visitor(this, visitor)
+    ! Visitor pattern support for error nodes (implements visit_interface)
+    subroutine error_node_accept(this, visitor)
         class(error_node_t), intent(in) :: this
-        class(*), intent(inout) :: visitor
+        class(ast_visitor_base_t), intent(inout) :: visitor
         ! Error nodes don't participate in normal AST visitation
         ! They represent failure states that should be handled separately
     end subroutine
