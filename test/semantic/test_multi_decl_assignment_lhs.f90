04ae8c6c91901fab456bfa3054d9abbeb06dadb8
Christopher Albert <albert@tugraz.at>
2025-09-02 05:01:59 +0200

    fix: semantic: false undefined-variable in assignment for declared vars (fixes #1159) (#1160)
    * fix: avoid false undefined-variable errors from assignment inference (fixes #1159)
    
    * test(semantic): add regression for multi-declaration LHS assignment (fixes #1159)

---
+++ test/semantic/test_multi_decl_assignment_lhs.f90
@@ -0,0 +1,25 @@
+program test_multi_decl_assignment_lhs
+    use frontend
+    implicit none
+
+    character(len=:), allocatable :: src
+    character(len=:), allocatable :: out_code, err
+
+    print *, 'Testing LHS assignment for multi-declarations...'
+
+    src = 'program p' // new_line('a') // &
+          '  implicit none' // new_line('a') // &
+          '  real :: a = 1.0, b = 2.0, c = 3.0' // new_line('a') // &
+          '  b = b + c' // new_line('a') // &
+          'end program'
+
+    call transform_lazy_fortran_string(src, out_code, err)
+
+    if (len(err) /= 0) then
+        print *, 'ERROR: Unexpected diagnostic:', trim(err)
+        stop 1
+    end if
+
+    print *, 'SUCCESS: No undefined-variable error for LHS assignment'
+end program test_multi_decl_assignment_lhs
+
