624ff7502daa964d2b7c7abcb431330213d642ce
Christopher Albert <albert@tugraz.at>
2025-09-03 01:04:07 +0200

    test(codegen): cover deallocate preservation alongside allocate

--- test/codegen/test_basic_allocate_codegen.f90
+++ test/codegen/test_basic_allocate_codegen.f90
@@ -11,20 +11,21 @@ program test_basic_allocate_codegen
         'integer, parameter :: n = 100' // new_line('a') // &
         'integer, dimension(n) :: arr'   // new_line('a') // &
         'integer, allocatable :: dyn_arr(:)' // new_line('a') // &
-        'allocate(dyn_arr(n))' // new_line('a')
+        'allocate(dyn_arr(n))' // new_line('a') // &
+        'deallocate(dyn_arr)'
 
     call transform_lazy_fortran_string(source, output, error_msg)
 
-    ok = index(output, 'allocate(dyn_arr(n))') > 0
+    ok = index(output, 'allocate(dyn_arr(n))') > 0 .and. &
+         index(output, 'deallocate(dyn_arr)') > 0
 
     if (.not. ok) then
-        print *, 'FAIL: allocate statement not preserved in output'
+        print *, 'FAIL: allocate/deallocate statement(s) not preserved in output'
         print *, 'Output:'
         print *, trim(output)
         stop 1
     end if
 
-    print *, 'PASS: allocate statement preserved'
+    print *, 'PASS: allocate/deallocate statements preserved'
     stop 0
 end program test_basic_allocate_codegen
-
