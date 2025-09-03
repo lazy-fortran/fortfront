c422e5eb9d95022d2c1dc048323e4f413b6ed448
Christopher Albert <albert@tugraz.at>
2025-09-03 10:36:37 +0200

    refactor: Remove dead code and obsolete files (#1216)
    * refactor: Remove dead code and obsolete comments
    
    - Remove 7 .broken files (type_utilities, ast_node_accessors, expression_temporaries, procedure_analysis, test files)
    - Clean up commented-out imports and function declarations in fortfront.f90 and fortfront_advanced.f90
    - Remove references to non-existent modules in documentation comments
    
    This removes over 1000 lines of confirmed dead code that was explicitly marked as broken or commented out.
    
    * refactor: Remove 7 broken files totaling over 1000 lines
    
    These .broken files were explicitly marked as non-functional:
    - src/semantic/types/type_utilities.f90.broken (371 lines)
    - src/ast/ast_node_accessors.f90.broken (547 lines)
    - src/analysis/expression_temporaries.f90.broken
    - src/analysis/procedure_analysis.f90.broken
    - test/api/test_node_accessor_functions.f90.broken
    - test/api/test_symbol_table_api.f90.broken
    - test/semantic/test_expression_temporary_tracking.f90.broken
    
    Total: 1000+ lines of confirmed dead code removed.

--- src/fortfront_advanced.f90
+++ src/fortfront_advanced.f90
@@ -1,39 +1,17 @@
 module fortfront_advanced
     ! fortfront Advanced Functions - Main interface module
     ! This module re-exports functionality from specialized modules:
-    ! - ast_node_accessors: Type-safe AST node accessors
-    ! - semantic_query_api: Symbol table and scope analysis
-    ! - type_utilities: Type system utilities and intrinsics
-    ! - expression_temporaries: Expression temporary tracking
     ! - call_graph_analysis: Call graph building and analysis
     ! - control_flow_analysis: Control flow graph functions
-    ! - procedure_analysis: Procedure and parameter analysis
     
     ! Re-export all functionality from specialized modules
-    ! NOTE: Multiple modules temporarily disabled due to API mismatch
-    ! use ast_node_accessors
-    ! use semantic_query_api
-    ! use type_utilities
-    ! use expression_temporaries
     use call_graph_analysis
     use control_flow_analysis
-    ! use procedure_analysis
     
     implicit none
     private
     
     ! Re-export all public interfaces
-    ! NOTE: ast_node_accessors functions temporarily disabled
-    ! public :: get_assignment_indices, get_binary_op_info, get_identifier_name, &
-    !           get_literal_value, get_call_info, get_array_literal_info, &
-    !           get_program_info, get_declaration_info, get_parameter_declaration_info, &
-    !           get_declaration_details, get_parameter_declaration_details, &
-    ! NOTE: semantic_query_api functions are provided by semantic_query_api
-    ! public :: get_symbol_info, get_symbols_in_scope, get_symbol_references, &
-    !           get_scope_info, get_all_scopes, &
-    ! NOTE: expression_temporaries functions temporarily disabled
-    ! public :: get_expression_temporaries, get_temporary_info, &
-    !           get_active_temporary_count, get_total_temporary_count, &
     public :: build_call_graph_from_arena, get_unused_procedures, &
               get_procedure_callers, get_procedure_callees, &
               is_procedure_used, get_all_procedures_in_graph, &
@@ -44,16 +22,6 @@ module fortfront_advanced
               get_cfg_block_successors, is_cfg_block_reachable, &
               get_cfg_unreachable_statements, print_control_flow_graph, &
               export_cfg_to_dot
-    ! NOTE: procedure_analysis functions temporarily disabled
-    ! count_procedure_parameters, get_parameter_intent, get_parameter_type, &
-    ! is_parameter_optional, get_procedure_signature, &
-    ! get_procedure_references, is_procedure_used_in_generic, &
-    ! get_parameter_usage_context
-    ! NOTE: type_utilities functions temporarily disabled 
-    ! lookup_symbol, get_scope_symbols, mono_type_to_type_info, &
-    ! get_type_info_for_base_type, get_integer_literal_value, &
-    ! get_real_literal_value, get_string_literal_value, &
-    ! is_intrinsic_function, get_intrinsic_signature, get_type_string
 
 contains
     
