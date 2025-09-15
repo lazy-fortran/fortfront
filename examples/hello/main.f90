16328afbc9fc7c56e8004db95940910eda9aa03f
Christopher Albert <albert@tugraz.at>
2025-09-15 08:20:07 +0200

    ci(windows): add smoke run of fortfront --version and hello.exe example to isolate pre-main crashes

---
+++ examples/hello/main.f90
@@ -0,0 +1,7 @@
+program hello
+    use, intrinsic :: iso_fortran_env, only: output_unit
+    implicit none
+    write(output_unit,'(A)') 'HELLO: start'
+    write(output_unit,'(A)') 'HELLO: ok'
+end program hello
+
