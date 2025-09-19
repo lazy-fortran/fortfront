f4f5d8a8697f8c672e1b261e761f1ff0e1d24003
Christopher Albert <albert@tugraz.at>
2025-09-19 18:03:40 +0200

    fix(utils): prevent infinite loop on oversized input in cli_io (fixes #1320)

---
+++ test/utils/test_cli_io_large_input.f90
@@ -0,0 +1,36 @@
+program test_cli_io_large_input
+    use, intrinsic :: iso_fortran_env, only: error_unit
+    use cli_io, only: read_all_stdin_or_file
+    implicit none
+
+    character(len=:), allocatable :: text
+    integer :: status
+    character(len=*), parameter :: fname = 'tmp_large_input.txt'
+    integer :: u, i
+    character(len=10240) :: line
+
+    line = repeat('a', len(line))
+
+    open(newunit=u, file=fname, status='replace', action='write')
+    do i = 1, 1024
+        write(u, '(A)') line
+    end do
+    close(u)
+
+    call read_all_stdin_or_file(.true., fname, text, status)
+
+    if (status == 4) then
+        print *, 'PASS: Large input correctly reported as too large'
+    else
+        print *, 'FAIL: Expected status=4 for too large input, got', status
+        if (allocated(text)) then
+            print *, 'INFO: Partial text length =', len(text)
+        end if
+        stop 1
+    end if
+
+    ! Cleanup
+    open(newunit=u, file=fname, status='old', action='read')
+    close(u, status='delete')
+end program test_cli_io_large_input
+
