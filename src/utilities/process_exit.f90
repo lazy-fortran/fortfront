e8ac4e03f3bf012fdabc5072e95a0ff3b2384d8f
Christopher Albert <albert@tugraz.at>
2025-09-19 16:34:55 +0200

    fix(cli): use quiet exit instead of STOP to avoid stray 'STOP n' messages (fixes #1312)

---
+++ src/utilities/process_exit.f90
@@ -0,0 +1,22 @@
+module process_exit
+    use, intrinsic :: iso_c_binding, only: c_int
+    implicit none
+    private
+    public :: exit_quiet
+
+    interface
+        subroutine c_exit(status) bind(C, name="exit")
+            import :: c_int
+            integer(c_int), value :: status
+        end subroutine c_exit
+    end interface
+
+contains
+
+    subroutine exit_quiet(status)
+        integer, intent(in) :: status
+        call c_exit(int(status, kind=c_int))
+    end subroutine exit_quiet
+
+end module process_exit
+
