d440171ae09535b47a025042f06a35a0e42f47a1
Christopher Albert <albert@tugraz.at>
2025-10-20 14:59:59 +0200

    fix(codegen): propagate save attribute to codegen

--- src/codegen/codegen_declarations_core.f90
+++ src/codegen/codegen_declarations_core.f90
@@ -185,6 +185,7 @@ contains
         attr_info%is_target = node%is_target
         attr_info%is_external = node%is_external
         attr_info%is_parameter = node%is_parameter
+        attr_info%is_save = node%is_save
     end subroutine populate_declaration_attributes
 
     function build_declaration_entity_list(arena, node, has_dimension_attr) &
