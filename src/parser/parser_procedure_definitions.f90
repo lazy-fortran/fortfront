3bf873a4bd50a270ee9acbc1cd8b73ba12c91984
Christopher Albert <albert@tugraz.at>
2025-10-21 18:52:06 +0200

    fix: interface block incorrectly moved inside program body (fixes #1566)
    Interface blocks in program specifications were being stripped of their
    interface/end interface wrapper and incorrectly placed in the executable
    section.
    
    Changes:
    - Parser: Added support for parsing interface procedures (subroutine/function
      signatures within interface blocks) in parser_interface_blocks.f90
    - Parser: Registered interface procedure parser callback in dispatcher
    - Parser: Added interface block handling to program body parser
    - Codegen: Added interface block categorization and generation in program
      header assembly with proper indentation
    - Codegen: Fixed implicit none literal nodes to be treated as header
      statements to preserve correct ordering
    - Test: Added comprehensive test for issue #1566
    
    The fix ensures interface blocks are:
    1. Parsed correctly with their procedure signatures
    2. Placed in the specification section before executable statements
    3. Generated with proper interface/end interface wrappers
    4. Indented correctly

--- src/parser/parser_procedure_definitions.f90
+++ src/parser/parser_procedure_definitions.f90
@@ -9,15 +9,128 @@ module parser_procedure_definitions_module
         merge_parameter_attributes_if_needed, ensure_recursive_prefix, &
         parse_subroutine_header
     use parser_procedure_definition_bodies_module, only: parse_procedure_body
-    use parser_interface_blocks_module, only: parse_interface_block
+    use parser_interface_blocks_module, only: parse_interface_block, &
+                                              set_interface_procedure_parser
+    use string_utils_mod, only: to_lower
+    use lexer_core, only: token_t, TK_KEYWORD, TK_IDENTIFIER, TK_NEWLINE, TK_WHITESPACE
     implicit none
     private
 
     public :: parse_function_definition
     public :: parse_subroutine_definition
     public :: parse_interface_block
+    public :: init_interface_procedure_parser
+
+    logical :: parser_initialized = .false.
 
 contains
+
+    subroutine init_interface_procedure_parser()
+        if (.not. parser_initialized) then
+            call set_interface_procedure_parser(parse_interface_procedure_impl)
+            parser_initialized = .true.
+        end if
+    end subroutine init_interface_procedure_parser
+
+    function parse_interface_procedure_impl(parser, arena, prefix_buffer) &
+        result(proc_index)
+        type(parser_state_t), intent(inout) :: parser
+        type(ast_arena_t), intent(inout) :: arena
+        type(parser_prefix_buffer_t), intent(inout) :: prefix_buffer
+        integer :: proc_index
+
+        type(token_t) :: token
+        character(len=:), allocatable :: lowered_text
+
+        proc_index = 0
+        token = parser%peek()
+
+        if (token%kind == TK_KEYWORD) then
+            lowered_text = to_lower(token%text)
+            if (trim(lowered_text) == "subroutine") then
+                proc_index = parse_interface_subroutine(parser, arena, prefix_buffer)
+            else if (trim(lowered_text) == "function") then
+                proc_index = parse_interface_function(parser, arena, prefix_buffer)
+            end if
+        end if
+    end function parse_interface_procedure_impl
+
+    function parse_interface_subroutine(parser, arena, prefix_buffer) &
+        result(sub_index)
+        type(parser_state_t), intent(inout) :: parser
+        type(ast_arena_t), intent(inout) :: arena
+        type(parser_prefix_buffer_t), intent(inout) :: prefix_buffer
+        integer :: sub_index
+
+        character(len=:), allocatable :: subroutine_name
+        integer :: line, column
+        integer, allocatable :: param_indices(:), body_indices(:)
+        character(len=16), allocatable :: prefix_keywords(:)
+        logical :: has_recursive_keyword
+        logical :: infer_recursive_from_body
+
+        has_recursive_keyword = .false.
+        infer_recursive_from_body = .false.
+
+        call parse_function_prefix_keywords(parser, prefix_buffer, &
+                                            prefix_keywords=prefix_keywords, &
+                                            has_recursive_keyword=has_recursive_keyword)
+        call parse_subroutine_header(parser, subroutine_name, line, column)
+        call parse_parameter_list(parser, arena, param_indices)
+        call parse_procedure_body(parser, arena, subroutine_name, "subroutine", &
+                                  body_indices, infer_recursive_from_body)
+
+        call merge_parameter_attributes_if_needed(arena, param_indices, &
+                                                  body_indices)
+
+        sub_index = push_subroutine_def(arena, subroutine_name, param_indices, &
+                                        body_indices, line, column, &
+                                        is_recursive=has_recursive_keyword, &
+                                        prefix_keywords=prefix_keywords)
+    end function parse_interface_subroutine
+
+    function parse_interface_function(parser, arena, prefix_buffer) &
+        result(func_index)
+        type(parser_state_t), intent(inout) :: parser
+        type(ast_arena_t), intent(inout) :: arena
+        type(parser_prefix_buffer_t), intent(inout) :: prefix_buffer
+        integer :: func_index
+
+        character(len=:), allocatable :: function_name, return_type_str, &
+                                         result_variable_name
+        integer :: line, column
+        integer, allocatable :: param_indices(:), body_indices(:)
+        logical :: has_recursive_keyword, is_valid, infer_recursive_from_body
+        character(len=16), allocatable :: prefix_keywords(:)
+
+        infer_recursive_from_body = .false.
+
+        call parse_function_prefix_keywords(parser, prefix_buffer, &
+                                            prefix_keywords=prefix_keywords, &
+                                            has_recursive_keyword=has_recursive_keyword)
+
+        call parse_function_signature(parser, return_type_str, function_name, &
+                                      line, column, is_valid)
+        if (.not. is_valid) then
+            func_index = 0
+            return
+        end if
+
+        call parse_parameter_list(parser, arena, param_indices)
+        call parse_function_result_clause(parser, result_variable_name)
+        call parse_procedure_body(parser, arena, function_name, "function", &
+                                  body_indices, infer_recursive_from_body)
+
+        call merge_parameter_attributes_if_needed(arena, param_indices, &
+                                                  body_indices)
+
+        func_index = push_function_def(arena, function_name, param_indices, &
+                                       return_type_str, body_indices, &
+                                       line, column, &
+                                       result_variable=result_variable_name, &
+                                       is_recursive=has_recursive_keyword, &
+                                       prefix_keywords=prefix_keywords)
+    end function parse_interface_function
 
     function parse_function_definition(parser, arena, prefix_buffer, prefix_list) &
         result(func_index)
