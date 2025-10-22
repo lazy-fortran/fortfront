06b8d54ba87b1247c307704d934389cc5e179ca8
Christopher Albert <albert@tugraz.at>
2025-10-23 00:53:49 +0200

    fix(parser): accept uppercase open close

--- src/parser/parser_io_statements.f90
+++ src/parser/parser_io_statements.f90
@@ -2,7 +2,8 @@ module parser_io_statements_module
     ! Parser module for I/O statement types (print, write, read)
     use, intrinsic :: iso_fortran_env, only: error_unit
     use lexer_core, only: token_t, TK_EOF, TK_IDENTIFIER, TK_NUMBER, TK_STRING, &
-                          TK_OPERATOR, TK_KEYWORD, TK_NEWLINE, TK_COMMENT, TK_WHITESPACE
+                          TK_OPERATOR, TK_KEYWORD, TK_NEWLINE, TK_COMMENT, &
+                          TK_WHITESPACE, to_lower
     use parser_state_module
     use parser_expressions_module, only: parse_comparison
     use ast_arena_modern, only: ast_arena_t
@@ -481,11 +482,12 @@ contains
         integer :: open_index
         type(token_t) :: token
         integer :: line, column
-        character(len=:), allocatable :: key, value
         character(len=:), allocatable :: spec_text
+        character(len=:), allocatable :: lowered
 
         token = parser%peek()
-        if (token%kind /= TK_KEYWORD .or. token%text /= "open") then
+        lowered = trim(to_lower(token%text))
+        if (token%kind /= TK_KEYWORD .or. lowered /= "open") then
             open_index = 0
             return
         end if
@@ -530,9 +532,11 @@ contains
         type(token_t) :: token
         integer :: line, column
         character(len=:), allocatable :: spec_text
+        character(len=:), allocatable :: lowered
 
         token = parser%peek()
-        if (token%kind /= TK_KEYWORD .or. token%text /= "close") then
+        lowered = trim(to_lower(token%text))
+        if (token%kind /= TK_KEYWORD .or. lowered /= "close") then
             close_index = 0
             return
         end if
