b67baf8c5e3aa730db873e7eda911b39352a512d
Christopher Albert <albert@tugraz.at>
2025-10-24 14:37:18 +0200

    test(codegen): refactor cmplx intrinsic test helper

--- test/codegen/test_issue_1811_cmplx_intrinsic.f90
+++ test/codegen/test_issue_1811_cmplx_intrinsic.f90
@@ -23,21 +23,7 @@ contains
         character(len=:), allocatable :: generated
         character(len=:), allocatable :: errors
 
-        source = 'program test_complex_numbers' // new_line('a') // &
-                 '    implicit none' // new_line('a') // &
-                 '    complex :: z1, z2, z3' // new_line('a') // &
-                 '    ' // new_line('a') // &
-                 '    z1 = (1.0, 2.0)' // new_line('a') // &
-                 '    z2 = cmplx(3.0, 4.0)' // new_line('a') // &
-                 '    z3 = z1 + z2' // new_line('a') // &
-                 '    ' // new_line('a') // &
-                 '    print *, z1' // new_line('a') // &
-                 '    print *, z2' // new_line('a') // &
-                 '    print *, z3' // new_line('a') // &
-                 '    print *, real(z3)' // new_line('a') // &
-                 '    print *, aimag(z3)' // new_line('a') // &
-                 'end program test_complex_numbers'
-
+        call build_cmplx_intrinsic_source(source)
         call transform_lazy_fortran_string(source, generated, errors)
 
         if (.not. allocated(generated)) generated = ''
@@ -72,5 +58,22 @@ contains
             passed = .false.
         end if
     end function run_cmplx_intrinsic_case
+
+    subroutine build_cmplx_intrinsic_source(source)
+        character(len=:), allocatable, intent(out) :: source
+
+        source = 'program test_complex_numbers' // new_line('a') // &
+                 '    implicit none' // new_line('a') // &
+                 '    complex :: z1, z2, z3' // new_line('a') // &
+                 '    z1 = (1.0, 2.0)' // new_line('a') // &
+                 '    z2 = cmplx(3.0, 4.0)' // new_line('a') // &
+                 '    z3 = z1 + z2' // new_line('a') // &
+                 '    print *, z1' // new_line('a') // &
+                 '    print *, z2' // new_line('a') // &
+                 '    print *, z3' // new_line('a') // &
+                 '    print *, real(z3)' // new_line('a') // &
+                 '    print *, aimag(z3)' // new_line('a') // &
+                 'end program test_complex_numbers'
+    end subroutine build_cmplx_intrinsic_source
 
 end program test_issue_1811_cmplx_intrinsic
