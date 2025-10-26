ec3d01edf04731f67a7229ba343c2b84886f7aee
Christopher Albert <albert@tugraz.at>
2025-10-26 20:31:26 +0100

    refactor(semantic): split analyzer implementation (fixes #1923) (#1952)

---
+++ src/semantic/analyzers/semantic_analyzer_context_impl.f90
@@ -0,0 +1,232 @@
+submodule(semantic_analyzer) semantic_analyzer_context_impl
+    use type_system_unified, only: type_var_t, mono_type_t, poly_type_t, &
+                                   create_mono_type, create_fun_type, &
+                                   create_poly_type, &
+                                   TVAR, TREAL
+    use scope_manager, only: create_scope_stack
+    use parser_type_hooks_module, only: consume_type_annotations, &
+                                        has_type_annotations
+    use semantic_annotation_utils, only: type_from_annotation
+    use semantic_inference_helpers, only: check_implicit_none, &
+                                          process_declaration_variables
+    use semantic_undefined_variable_checker, only: check_undefined_variables_generic
+    use constant_transformation, only: fold_constants_in_arena
+    use error_handling, only: create_error_collection
+    use type_hierarchy, only: create_type_hierarchy
+    use call_graph_signatures_mod, only: create_signatures_map
+    use semantic_validation_utils, only: int_to_str
+    use ast_nodes_data, only: declaration_node
+    use semantic_context_types, only: semantic_context_base_t
+    implicit none
+contains
+
+    module subroutine create_semantic_context(ctx)
+        type(semantic_context_t), intent(out) :: ctx
+        type(poly_type_t) :: builtin_scheme
+        type(mono_type_t) :: real_to_real, real_type
+
+        ctx%context_id = 1
+        ctx%context_name = "semantic_context"
+
+        call create_scope_stack(ctx%scopes)
+        ctx%subst%count = 0
+        ctx%subst%capacity = 64
+        if (allocated(ctx%subst%vars)) deallocate (ctx%subst%vars)
+        if (allocated(ctx%subst%types)) deallocate (ctx%subst%types)
+        allocate (ctx%subst%vars(ctx%subst%capacity))
+        allocate (ctx%subst%types(ctx%subst%capacity))
+        ctx%errors = create_error_collection()
+        ctx%next_var_id = 1
+        ctx%respect_implicit_none = .true.
+        ctx%type_hierarchy = create_type_hierarchy()
+        ctx%signatures = create_signatures_map()
+
+        real_type = create_mono_type(TREAL)
+        real_to_real = create_fun_type(real_type, real_type)
+
+        builtin_scheme = create_poly_type(forall_vars=[type_var_t ::], &
+                                          mono=real_to_real)
+
+        call ctx%scopes%define("exp", builtin_scheme)
+
+        if (has_type_annotations()) then
+            call consume_type_annotations(ctx%parser_type_hints)
+        else
+            allocate (ctx%parser_type_hints(0))
+        end if
+        call ctx%scopes%define("log", builtin_scheme)
+        call ctx%scopes%define("abs", builtin_scheme)
+    end subroutine create_semantic_context
+
+    module subroutine analyze_program(ctx, arena, root_index)
+        type(semantic_context_t), intent(inout) :: ctx
+        type(ast_arena_t), intent(inout) :: arena
+        integer, intent(in) :: root_index
+
+        if (root_index <= 0 .or. root_index > arena%size) return
+        if (.not. allocated(arena%entries(root_index)%node)) return
+
+        select type (ast => arena%entries(root_index)%node)
+        type is (program_node)
+            if (ctx%respect_implicit_none .and. .not. ctx%strict_mode) then
+                ctx%strict_mode = check_implicit_none(arena, ast)
+            end if
+            call analyze_program_node_arena(ctx, arena, ast, root_index)
+        type is (module_node)
+            return
+        class default
+            call infer_and_store_type(ctx, arena, root_index)
+        end select
+
+        call fold_constants_in_arena(arena)
+    end subroutine analyze_program
+
+    module subroutine analyze_program_node_arena(ctx, arena, prog, prog_index)
+        type(semantic_context_t), intent(inout) :: ctx
+        type(ast_arena_t), intent(inout) :: arena
+        type(program_node), intent(inout) :: prog
+        integer, intent(in) :: prog_index
+        integer :: i
+
+        if (allocated(prog%body_indices)) then
+            do i = 1, size(prog%body_indices)
+                if (prog%body_indices(i) > 0 .and. &
+                    prog%body_indices(i) <= arena%size) then
+                    if (allocated(arena%entries(prog%body_indices(i))%node)) then
+                        select type (node => arena%entries(prog%body_indices(i))%node)
+                        type is (declaration_node)
+                            block
+                                type(mono_type_t) :: decl_type
+                                type(poly_type_t) :: scheme
+                                type(type_annotation_t) :: hint
+                                integer :: j
+
+                                if (ctx%get_type_hint(prog%body_indices(i), hint)) then
+                                    call type_from_annotation(hint, decl_type)
+                                else
+                                    call process_declaration_variables(node, decl_type)
+                                end if
+
+                                scheme = ctx%generalize(decl_type)
+                                if (node%is_multi_declaration .and. &
+                                    allocated(node%var_names)) then
+                                    do j = 1, size(node%var_names)
+                                        call ctx%scopes%define(node%var_names(j), &
+                                                               scheme)
+                                    end do
+                                else if (allocated(node%var_name)) then
+                                    call ctx%scopes%define(node%var_name, scheme)
+                                end if
+                            end block
+                        class default
+                            continue
+                        end select
+                    end if
+                end if
+            end do
+        end if
+
+        if (allocated(prog%body_indices)) then
+            do i = 1, size(prog%body_indices)
+                if (prog%body_indices(i) > 0 .and. &
+                    prog%body_indices(i) <= arena%size) then
+                    call infer_and_store_type(ctx, arena, prog%body_indices(i))
+                end if
+            end do
+        end if
+        call check_undefined_variables_generic(ctx%scopes, ctx%errors, &
+                                               ctx%strict_mode, arena, prog_index)
+    end subroutine analyze_program_node_arena
+
+    module subroutine infer_and_store_type(ctx, arena, node_index)
+        type(semantic_context_t), intent(inout) :: ctx
+        type(ast_arena_t), intent(inout) :: arena
+        integer, intent(in) :: node_index
+        type(mono_type_t) :: inferred
+
+        if (node_index <= 0 .or. node_index > arena%size) return
+        if (.not. allocated(arena%entries(node_index)%node)) return
+
+        inferred = ctx%infer_stmt(arena, node_index)
+    end subroutine infer_and_store_type
+
+    module subroutine set_node_inferred_type(arena, index, typ)
+        type(ast_arena_t), intent(inout) :: arena
+        integer, intent(in) :: index
+        type(mono_type_t), intent(in) :: typ
+
+        if (index <= 0 .or. index > arena%size) return
+        if (.not. allocated(arena%entries(index)%node)) return
+        arena%entries(index)%node%inferred_type = typ
+    end subroutine set_node_inferred_type
+
+    module function get_inferred_type_from_arena(ctx, arena, index) result(typ)
+        class(semantic_context_t), intent(inout) :: ctx
+        type(ast_arena_t), intent(inout) :: arena
+        integer, intent(in) :: index
+        type(mono_type_t) :: typ
+
+        typ = create_mono_type(TREAL)
+        if (index <= 0 .or. index > arena%size) return
+        if (.not. allocated(arena%entries(index)%node)) return
+
+        typ = ctx%apply_subst_to_type(arena%entries(index)%node%inferred_type)
+        if (typ%kind == TVAR) then
+            if (len_trim(typ%var%name) == 0) typ%var%name = "v" // &
+                                                            int_to_str(typ%var%id)
+        end if
+        arena%entries(index)%node%inferred_type = typ
+    end function get_inferred_type_from_arena
+
+    module function has_semantic_errors(ctx) result(has_errors)
+        type(semantic_context_t), intent(in) :: ctx
+        logical :: has_errors
+        has_errors = ctx%errors%has_errors()
+    end function has_semantic_errors
+
+    module function semantic_get_context_name(this) result(name)
+        class(semantic_context_t), intent(in) :: this
+        character(:), allocatable :: name
+        name = "semantic_context"
+    end function semantic_get_context_name
+
+    module function semantic_clone_context(this) result(cloned)
+        class(semantic_context_t), intent(in) :: this
+        class(semantic_context_base_t), allocatable :: cloned
+        type(semantic_context_t) :: temp_context
+
+        temp_context%context_id = this%context_id
+        temp_context%context_name = this%context_name
+        temp_context%scopes = this%scopes
+        temp_context%next_var_id = this%next_var_id
+        temp_context%subst = this%subst
+        temp_context%errors = this%errors
+        temp_context%strict_mode = this%strict_mode
+        temp_context%respect_implicit_none = this%respect_implicit_none
+        temp_context%signatures = this%signatures
+
+        allocate (cloned, source=temp_context)
+    end function semantic_clone_context
+
+    module function semantic_get_type_hint(this, decl_index, annotation) &
+        result(found)
+        class(semantic_context_t), intent(in) :: this
+        integer, intent(in) :: decl_index
+        type(type_annotation_t), intent(out) :: annotation
+        integer :: i
+        logical :: found
+
+        found = .false.
+        if (.not. allocated(this%parser_type_hints)) return
+        if (size(this%parser_type_hints) == 0) return
+
+        do i = 1, size(this%parser_type_hints)
+            if (this%parser_type_hints(i)%decl_index == decl_index) then
+                annotation = this%parser_type_hints(i)
+                found = .true.
+                return
+            end if
+        end do
+    end function semantic_get_type_hint
+
+end submodule semantic_analyzer_context_impl
---
+++ src/semantic/analyzers/semantic_analyzer_type_ops_impl.f90
@@ -0,0 +1,104 @@
+submodule(semantic_analyzer) semantic_analyzer_type_ops_impl
+    use semantic_type_operations, only: instantiate_type_scheme_op, &
+                                        generalize_type_op, &
+                                        generate_fresh_type_var_op, &
+                                        apply_substitution_to_type
+    use type_system_unified, only: mono_type_t, poly_type_t, type_var_t, &
+                                   substitution_t, create_fun_type, create_mono_type, &
+                                   compose_substitutions, TREAL
+    implicit none
+contains
+
+    module subroutine unify_types(this, t1, t2)
+        class(semantic_context_t), intent(inout) :: this
+        type(mono_type_t), intent(in) :: t1
+        type(mono_type_t), intent(in) :: t2
+
+        ! Simplified unification retained for lean build
+    end subroutine unify_types
+
+    module function instantiate_type_scheme(this, scheme) result(typ)
+        class(semantic_context_t), intent(inout) :: this
+        type(poly_type_t), intent(in) :: scheme
+        type(mono_type_t) :: typ
+
+        typ = instantiate_type_scheme_op(scheme, this%next_var_id)
+    end function instantiate_type_scheme
+
+    module function generalize_type(this, typ) result(scheme)
+        class(semantic_context_t), intent(in) :: this
+        type(mono_type_t), intent(in) :: typ
+        type(poly_type_t) :: scheme
+
+        scheme = generalize_type_op(typ)
+    end function generalize_type
+
+    module function generate_fresh_type_var(this) result(tv)
+        class(semantic_context_t), intent(inout) :: this
+        type(type_var_t) :: tv
+
+        tv = generate_fresh_type_var_op(this%next_var_id)
+    end function generate_fresh_type_var
+
+    module function apply_current_substitution(this, typ) result(result_type)
+        class(semantic_context_t), intent(in) :: this
+        type(mono_type_t), intent(in) :: typ
+        type(mono_type_t) :: result_type
+
+        result_type = apply_substitution_to_type(typ, this%subst)
+    end function apply_current_substitution
+
+    module function get_builtin_function_type(this, name) result(typ)
+        class(semantic_context_t), intent(inout) :: this
+        character(len=*), intent(in) :: name
+        type(mono_type_t) :: typ
+        type(poly_type_t), allocatable :: scheme
+
+        call this%scopes%lookup(name, scheme)
+        if (allocated(scheme)) then
+            typ = this%instantiate(scheme)
+        else
+            typ = create_fun_type(create_mono_type(TREAL), create_mono_type(TREAL))
+        end if
+    end function get_builtin_function_type
+
+    module subroutine compose_with_subst(this, new_subst)
+        class(semantic_context_t), intent(inout) :: this
+        type(substitution_t), intent(in) :: new_subst
+
+        this%subst = compose_substitutions(new_subst, this%subst)
+    end subroutine compose_with_subst
+
+    module subroutine semantic_context_deep_copy(this, copy)
+        class(semantic_context_t), intent(in) :: this
+        type(semantic_context_t), intent(out) :: copy
+
+        copy%scopes = this%scopes
+        copy%next_var_id = this%next_var_id
+        copy%subst = this%subst
+        copy%errors = this%errors
+        copy%strict_mode = this%strict_mode
+        copy%respect_implicit_none = this%respect_implicit_none
+        copy%signatures = this%signatures
+    end subroutine semantic_context_deep_copy
+
+    module subroutine semantic_context_assign(lhs, rhs)
+        class(semantic_context_t), intent(inout) :: lhs
+        type(semantic_context_t), intent(in) :: rhs
+
+        lhs%scopes = rhs%scopes
+        lhs%next_var_id = rhs%next_var_id
+        lhs%subst = rhs%subst
+        lhs%errors = rhs%errors
+        lhs%strict_mode = rhs%strict_mode
+        lhs%respect_implicit_none = rhs%respect_implicit_none
+        lhs%signatures = rhs%signatures
+    end subroutine semantic_context_assign
+
+    module function semantic_context_has_errors(this) result(has_errors)
+        class(semantic_context_t), intent(in) :: this
+        logical :: has_errors
+        has_errors = this%errors%has_errors()
+    end function semantic_context_has_errors
+
+end submodule semantic_analyzer_type_ops_impl
