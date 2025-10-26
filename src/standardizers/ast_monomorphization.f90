84d10492f943be0f7c19f3faa31377f75504f9b3
Christopher Albert <albert@tugraz.at>
2025-10-26 07:13:39 +0100

    fix(monomorphization): correct return type propagation (fixes #1892)

--- src/standardizers/ast_monomorphization.f90
+++ src/standardizers/ast_monomorphization.f90
@@ -6,7 +6,7 @@ module ast_monomorphization
                                    get_procedure_body, get_procedure_return_type, &
                                    create_function_def
     use ast_nodes_data, only: module_node, parameter_declaration_node, &
-                              create_module, INTENT_IN
+                              declaration_node, create_module
     use ast_nodes_misc, only: interface_block_node, module_procedure_node, &
                               use_statement_node, create_interface_block, &
                               create_module_procedure, create_use_statement
@@ -14,8 +14,10 @@ module ast_monomorphization
     use call_graph_signatures_mod, only: signatures_map_t, type_signature_t
     use codegen_name_mangling, only: mangle_procedure_name
     use type_string_utils, only: mono_type_to_string
-    use type_system_unified, only: mono_type_t
+    use type_system_unified, only: mono_type_t, create_mono_type, TINT, TREAL, &
+                                   TLOGICAL, TCHAR, TCOMPLEX, TDOUBLE, TARRAY
     use uid_generator, only: generate_uid
+    use string_utils_mod, only: to_lower
     implicit none
     private
 
@@ -24,7 +26,6 @@ module ast_monomorphization
 contains
 
     subroutine transform_monomorphization(arena, root_index, signatures)
-        use, intrinsic :: iso_fortran_env, only: error_unit
         type(ast_arena_t), intent(inout) :: arena
         integer, intent(in) :: root_index
         type(signatures_map_t), intent(in) :: signatures
@@ -108,9 +109,12 @@ contains
         type(function_def_node), pointer :: orig_func
         type(function_def_node) :: new_func
         integer, allocatable :: new_param_indices(:)
+        integer, allocatable :: body_indices_copy(:)
         character(len=:), allocatable :: mangled_name, return_type
         character(len=:), allocatable :: result_name
+        character(len=:), allocatable :: lowered_return
         integer :: i
+        type(declaration_node) :: decl_copy
 
         call get_function_node(arena, func_idx, orig_func)
         if (.not. associated(orig_func)) then
@@ -124,8 +128,22 @@ contains
         if (allocated(orig_func%param_indices)) then
             allocate (new_param_indices(size(orig_func%param_indices)))
             do i = 1, size(orig_func%param_indices)
-                new_param_indices(i) = clone_parameter_with_kind(arena, &
-                                   orig_func%param_indices(i), signature%param_kinds(i))
+                if (allocated(signature%param_type_strings)) then
+                    if (size(signature%param_type_strings) >= i) then
+                        new_param_indices(i) = clone_parameter_with_kind( &
+                                               arena, orig_func%param_indices(i), &
+                                               signature%param_kinds(i), &
+                                               signature%param_type_strings(i))
+                    else
+                        new_param_indices(i) = clone_parameter_with_kind( &
+                                               arena, orig_func%param_indices(i), &
+                                               signature%param_kinds(i))
+                    end if
+                else
+                    new_param_indices(i) = clone_parameter_with_kind( &
+                                           arena, orig_func%param_indices(i), &
+                                           signature%param_kinds(i))
+                end if
             end do
         else
             allocate (new_param_indices(0))
@@ -151,27 +169,86 @@ contains
             result_name = mangled_name
         end if
 
+        if (len_trim(return_type) > 0) then
+            lowered_return = to_lower(return_type)
+        else
+            lowered_return = ""
+        end if
+
+        if (allocated(orig_func%body_indices)) then
+            allocate (body_indices_copy(size(orig_func%body_indices)))
+            do i = 1, size(orig_func%body_indices)
+                body_indices_copy(i) = orig_func%body_indices(i)
+                if (body_indices_copy(i) < 1) cycle
+                if (body_indices_copy(i) > arena%size) cycle
+                if (.not. allocated(arena%entries(body_indices_copy(i))%node)) cycle
+                select type (decl => arena%entries(body_indices_copy(i))%node)
+                type is (declaration_node)
+                    if (allocated(result_name)) then
+                        if (len_trim(result_name) > 0) then
+                            if (allocated(decl%var_name)) then
+                                if (trim(decl%var_name) == trim(result_name)) then
+                                    decl_copy = decl
+                                    decl_copy%uid = generate_uid()
+                                    if (len_trim(return_type) > 0) then
+                                        decl_copy%type_name = trim(return_type)
+                                    end if
+                                    decl_copy%has_kind = .false.
+                                    if (allocated(lowered_return)) then
+                                        if (index(lowered_return, &
+                                                  "allocatable") > 0) then
+                                            decl_copy%is_allocatable = .true.
+                                        else
+                                            decl_copy%is_allocatable = .false.
+                                        end if
+                                        if (index(lowered_return, &
+                                                  "dimension(") > 0) then
+                                            decl_copy%is_array = .true.
+                                        else
+                                            decl_copy%is_array = .false.
+                                        end if
+                                    else
+                                        decl_copy%is_allocatable = .false.
+                                        decl_copy%is_array = .false.
+                                    end if
+                                    call arena%push(decl_copy)
+                                    body_indices_copy(i) = arena%size
+                                end if
+                            end if
+                        end if
+                    end if
+                end select
+            end do
+        else
+            allocate (body_indices_copy(0))
+        end if
+
         new_func = create_function_def( &
                    name=mangled_name, &
                    param_indices=new_param_indices, &
                    return_type=return_type, &
-                   body_indices=orig_func%body_indices, &
+                   body_indices=body_indices_copy, &
                    line=orig_func%line, &
                    column=orig_func%column, &
                    result_variable=result_name)
+        new_func%inferred_type = orig_func%inferred_type
 
         call arena%push(new_func)
         new_idx = arena%size
     end function clone_function_with_signature
 
-    function clone_parameter_with_kind(arena, param_idx, kind_value) result(new_idx)
+    function clone_parameter_with_kind(arena, param_idx, kind_value, type_override) &
+        result(new_idx)
         type(ast_arena_t), intent(inout) :: arena
         integer, intent(in) :: param_idx
         integer, intent(in) :: kind_value
+        character(len=*), intent(in), optional :: type_override
         integer :: new_idx
         type(parameter_declaration_node), pointer :: orig_param
         type(parameter_declaration_node) :: new_param
         character(len=:), allocatable :: type_name
+        logical :: override_provided
+        logical :: is_array_kind
 
         call get_parameter_node(arena, param_idx, orig_param)
         if (.not. associated(orig_param)) then
@@ -179,27 +256,51 @@ contains
             return
         end if
 
-        type_name = get_kind_type_string(kind_value)
-        if (len_trim(type_name) == 0) then
-            if (allocated(orig_param%type_name)) then
-                if (len_trim(orig_param%type_name) > 0) then
-                    type_name = trim(orig_param%type_name)
-                end if
+        new_param = orig_param
+        new_param%uid = generate_uid()
+
+        override_provided = .false.
+        if (present(type_override)) then
+            if (len_trim(type_override) > 0) then
+                new_param%type_name = trim(type_override)
+                override_provided = .true.
             end if
         end if
-        if (len_trim(type_name) == 0) type_name = "real"
 
-        new_param%uid = generate_uid()
-        new_param%name = orig_param%name
-        new_param%type_name = type_name
-        new_param%kind_value = get_actual_kind_value(kind_value)
-        new_param%has_kind = .true.
-        new_param%intent_type = INTENT_IN
-        new_param%is_optional = orig_param%is_optional
-        new_param%is_target = orig_param%is_target
-        new_param%is_array = orig_param%is_array
-        if (allocated(orig_param%dimension_indices)) then
-            new_param%dimension_indices = orig_param%dimension_indices
+        if (.not. override_provided) then
+            type_name = get_kind_type_string(kind_value)
+            if (len_trim(type_name) == 0) then
+                if (allocated(orig_param%type_name)) then
+                    if (len_trim(orig_param%type_name) > 0) then
+                        type_name = trim(orig_param%type_name)
+                    end if
+                end if
+            end if
+            if (len_trim(type_name) == 0) type_name = "real"
+            new_param%type_name = trim(type_name)
+        end if
+
+        is_array_kind = (kind_value == TARRAY)
+        if (override_provided) then
+            new_param%has_kind = .false.
+            new_param%kind_value = 0
+            if (index(to_lower(new_param%type_name), "dimension(") > 0) then
+                if (allocated(new_param%dimension_indices)) then
+                    deallocate (new_param%dimension_indices)
+                end if
+                new_param%is_array = .false.
+            end if
+        else
+            if (is_array_kind) then
+                new_param%has_kind = .false.
+                new_param%kind_value = orig_param%kind_value
+            else
+                new_param%kind_value = get_actual_kind_value(kind_value)
+                new_param%has_kind = .true.
+            end if
+            if (allocated(orig_param%dimension_indices)) then
+                new_param%dimension_indices = orig_param%dimension_indices
+            end if
         end if
 
         call arena%push(new_param)
@@ -288,15 +389,24 @@ contains
 
         type_str = ""
 
-        if (signature%call_site_node > 0) then
-            call get_call_node(arena, signature%call_site_node, call_node)
-            if (associated(call_node)) then
-                inferred_type = call_node%inferred_type
-                call inferred_type%sync_from_arena()
-                type_str = trim(mono_type_to_string(inferred_type, &
-                                                    include_shape=.false., fallback=''))
-                if (len_trim(type_str) == 0) then
-                    type_str = get_kind_type_string(inferred_type%kind)
+        if (allocated(signature%return_type_string)) then
+            if (len_trim(signature%return_type_string) > 0) then
+                type_str = trim(signature%return_type_string)
+            end if
+        end if
+
+        if (len_trim(type_str) == 0) then
+            if (signature%call_site_node > 0) then
+                call get_call_node(arena, signature%call_site_node, call_node)
+                if (associated(call_node)) then
+                    inferred_type = call_node%inferred_type
+                    call inferred_type%sync_from_arena()
+                    type_str = trim(mono_type_to_string(inferred_type, &
+                                                        include_shape=.true., &
+                                                        fallback=''))
+                    if (len_trim(type_str) == 0) then
+                        type_str = get_kind_type_string(inferred_type%kind)
+                    end if
                 end if
             end if
         end if
@@ -309,6 +419,11 @@ contains
                     type_str = trim(orig_func%return_type)
                 end if
             end if
+        end if
+
+        if (len_trim(type_str) == 0 .and. associated(orig_func)) then
+            type_str = trim(mono_type_to_string(orig_func%inferred_type, &
+                                                include_shape=.true., fallback=''))
         end if
 
         if (len_trim(type_str) == 0) then
@@ -330,7 +445,73 @@ contains
         end if
 
         if (len_trim(type_str) == 0) type_str = "real"
+
+        call update_call_result_type(arena, signature%call_site_node, &
+                                     signature%return_kind, type_str)
     end function determine_return_type_string
+
+    subroutine update_call_result_type(arena, call_index, return_kind, type_str)
+        type(ast_arena_t), intent(inout) :: arena
+        integer, intent(in) :: call_index
+        integer, intent(in) :: return_kind
+        character(len=*), intent(in) :: type_str
+        integer :: resolved_kind
+        type(call_or_subscript_node), pointer :: call_node
+
+        if (call_index <= 0 .or. call_index > arena%size) return
+        if (.not. allocated(arena%entries(call_index)%node)) return
+        call_node => null()
+        select type (node => arena%entries(call_index)%node)
+        type is (call_or_subscript_node)
+            call_node => node
+        class default
+            return
+        end select
+        if (.not. associated(call_node)) return
+
+        resolved_kind = return_kind
+        if (resolved_kind <= 0) then
+            resolved_kind = get_kind_from_type_string(type_str)
+        end if
+        if (resolved_kind <= 0) return
+        if (resolved_kind == TARRAY) return
+
+        call_node%inferred_type = create_mono_type(resolved_kind)
+    end subroutine update_call_result_type
+
+    integer function get_kind_from_type_string(type_name) result(kind_value)
+        character(len=*), intent(in) :: type_name
+        character(len=:), allocatable :: lowered
+
+        lowered = to_lower(trim(type_name))
+
+        select case (lowered)
+        case ('integer')
+            kind_value = TINT
+        case ('real')
+            kind_value = TREAL
+        case ('logical')
+            kind_value = TLOGICAL
+        case ('complex')
+            kind_value = TCOMPLEX
+        case ('real(8)')
+            kind_value = TDOUBLE
+        case ('double precision')
+            kind_value = TDOUBLE
+        case default
+            if (index(lowered, 'character') == 1) then
+                kind_value = TCHAR
+            else if (index(lowered, 'real(8)') == 1) then
+                kind_value = TDOUBLE
+            else if (index(lowered, 'real(') == 1) then
+                kind_value = TREAL
+            else if (index(lowered, 'integer') == 1) then
+                kind_value = TINT
+            else
+                kind_value = 0
+            end if
+        end select
+    end function get_kind_from_type_string
 
     function fallback_return_type_from_params(param_kinds) result(type_str)
         integer, intent(in) :: param_kinds(:)
