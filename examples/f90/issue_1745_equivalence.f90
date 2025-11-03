916af042c6b276b60693e05674ce17ad0fca4acd
Christopher Albert <albert@tugraz.at>
2025-11-03 16:20:18 +0100

    fix: create missing example files for tests
    - Created examples/f90/issue_1578_block_data.f90
    - Created examples/f90/issue_1745_equivalence.f90
    - Created examples/f90/issue_1810_parameter_array_allocatable.f90
    
    These example files were referenced by tests but never created
    when commit ee2e3dd0 converted tests to use read_example().

---
+++ examples/f90/issue_1745_equivalence.f90
@@ -0,0 +1,11 @@
+program test_equivalence
+    implicit none
+    integer :: i
+    real :: r
+    equivalence (i, r)
+    
+    i = 42
+    print *, 'i =', i
+    print *, 'r =', r
+    
+end program test_equivalence
