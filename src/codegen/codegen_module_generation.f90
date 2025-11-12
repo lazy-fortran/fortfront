bb1e3923f54193151984df4e3ae3fea0249d82cf
Christopher Albert <albert@tugraz.at>
2025-11-12 02:52:12 +0100

    fix: restrict module procedure emission outside modules

--- src/codegen/codegen_module_generation.f90
+++ src/codegen/codegen_module_generation.f90
@@ -328,8 +328,13 @@ contains
         integer :: i
         character(len=:), allocatable :: name_text
         logical :: first_name
+        logical :: allow_module_prefix
 
-        if (node%has_module_prefix) then
+        allow_module_prefix = node%has_module_prefix .and. &
+                              (in_module_context .or. &
+                               in_operator_or_assignment_interface)
+
+        if (allow_module_prefix) then
             code = "module procedure"
         else
             code = "procedure"
