1cbe03368bc0e1fd9a846a6a307aa2da216b5b61
Christopher Albert <albert@tugraz.at>
2025-11-14 04:29:01 +0100

    fix: split parser interface blocks helpers (fixes #2312) (#2357)

---
+++ src/parser/declarations/parser_interface_block_headers_module.f90
@@ -0,0 +1,87 @@
+module parser_interface_block_headers_module
+    use string_utils_mod, only: to_lower
+    use lexer_core, only: token_t, TK_KEYWORD, TK_IDENTIFIER, TK_OPERATOR
+    use parser_state_module, only: parser_state_t
+    implicit none
+    private
+    public :: begin_interface_block
+    public :: handle_interface_end
+contains
+
+    subroutine begin_interface_block(parser, interface_name, interface_kind, &
+                                     operator_symbol, line, column, is_abstract)
+        type(parser_state_t), intent(inout) :: parser
+        character(len=:), allocatable, intent(out) :: interface_name
+        character(len=:), allocatable, intent(out) :: interface_kind
+        character(len=:), allocatable, intent(out) :: operator_symbol
+        integer, intent(out) :: line, column
+        logical, intent(in), optional :: is_abstract
+
+        type(token_t) :: token
+        character(len=:), allocatable :: lowered
+
+        token = parser%consume()
+        line = token%line
+        column = token%column
+
+        interface_kind = "interface"
+        operator_symbol = ""
+
+        token = parser%peek()
+        if (token%kind == TK_IDENTIFIER .or. token%kind == TK_KEYWORD) then
+            lowered = to_lower(trim(token%text))
+            if (trim(lowered) == "operator" .or. trim(lowered) == "assignment") then
+                interface_kind = trim(lowered)
+                token = parser%consume()
+                token = parser%peek()
+                if (token%kind == TK_OPERATOR .and. trim(token%text) == "(") then
+                    token = parser%consume()
+                    token = parser%peek()
+                    operator_symbol = trim(token%text)
+                    token = parser%consume()
+                    token = parser%peek()
+                    if (token%kind == TK_OPERATOR .and. trim(token%text) == ")") then
+                        token = parser%consume()
+                    end if
+                end if
+                interface_name = ""
+            else
+                token = parser%consume()
+                interface_name = token%text
+            end if
+        else
+            interface_name = ""
+        end if
+    end subroutine begin_interface_block
+
+    logical function handle_interface_end(parser, first_token) result(is_end)
+        type(parser_state_t), intent(inout) :: parser
+        type(token_t), intent(in) :: first_token
+
+        type(token_t) :: next_token
+        character(len=:), allocatable :: lowered_text
+
+        is_end = .false.
+        if (first_token%kind /= TK_KEYWORD) return
+
+        lowered_text = to_lower(first_token%text)
+        if (trim(lowered_text) /= "end") return
+
+        next_token = parser%get_token_at_index(parser%current_token + 1)
+        if (next_token%kind /= TK_KEYWORD) return
+
+        lowered_text = to_lower(next_token%text)
+        if (trim(lowered_text) /= "interface") return
+
+        next_token = parser%consume()
+        next_token = parser%consume()
+
+        next_token = parser%peek()
+        if (next_token%kind == TK_IDENTIFIER) then
+            next_token = parser%consume()
+        end if
+
+        is_end = .true.
+    end function handle_interface_end
+
+end module parser_interface_block_headers_module
---
+++ src/parser/declarations/parser_interface_import_module.f90
@@ -0,0 +1,97 @@
+module parser_interface_import_module
+    use ast_arena_modern, only: ast_arena_t
+    use ast_factory, only: push_import_statement
+    use parser_state_module, only: parser_state_t
+    use string_utils_mod, only: to_lower
+    use lexer_core, only: token_t, TK_KEYWORD, TK_IDENTIFIER, TK_OPERATOR, TK_WHITESPACE
+    implicit none
+    private
+    public :: parse_import_statement
+contains
+
+    function parse_import_statement(parser, arena) result(stmt_index)
+        type(parser_state_t), intent(inout) :: parser
+        type(ast_arena_t), intent(inout) :: arena
+        integer :: stmt_index
+
+        character(len=:), allocatable :: import_names(:)
+        logical :: has_double_colon, is_all, is_none
+        integer :: line, column
+        type(token_t) :: token
+        character(len=:), allocatable :: lowered_text
+        integer :: name_count
+
+        stmt_index = 0
+        has_double_colon = .false.
+        is_all = .false.
+        is_none = .false.
+        name_count = 0
+        allocate (character(len=100) :: import_names(0))
+
+        token = parser%consume()
+        line = token%line
+        column = token%column
+
+        token = parser%peek()
+        if (token%kind == TK_OPERATOR .and. token%text == ",") then
+            token = parser%consume()
+            token = parser%peek()
+            if (token%kind == TK_KEYWORD) then
+                lowered_text = to_lower(token%text)
+                if (trim(lowered_text) == "all") then
+                    is_all = .true.
+                    token = parser%consume()
+                else if (trim(lowered_text) == "none") then
+                    is_none = .true.
+                    token = parser%consume()
+                end if
+            end if
+        else if (token%kind == TK_OPERATOR .and. token%text == "::") then
+            has_double_colon = .true.
+            token = parser%consume()
+        end if
+
+        if (.not. is_all .and. .not. is_none) then
+            do while (.not. parser%is_at_end())
+                token = parser%peek()
+                select case (token%kind)
+                case (TK_IDENTIFIER, TK_KEYWORD)
+                    name_count = name_count + 1
+                    block
+                        character(len=:), allocatable :: tmp_names(:)
+                        integer :: i
+                        allocate (character(len=100) :: tmp_names(name_count))
+                        do i = 1, name_count - 1
+                            tmp_names(i) = import_names(i)
+                        end do
+                        tmp_names(name_count) = trim(token%text)
+                        call move_alloc(tmp_names, import_names)
+                    end block
+                    token = parser%consume()
+                case (TK_OPERATOR)
+                    if (trim(token%text) == ",") then
+                        token = parser%consume()
+                    else
+                        exit
+                    end if
+                case (TK_WHITESPACE)
+                    token = parser%consume()
+                case default
+                    exit
+                end select
+            end do
+        end if
+
+        if (name_count > 0) then
+            stmt_index = push_import_statement(arena, import_names(1:name_count), &
+                                               has_double_colon=has_double_colon, &
+                                               line=line, column=column)
+        else
+            stmt_index = push_import_statement(arena, &
+                                               has_double_colon=has_double_colon, &
+                                               is_all=is_all, is_none=is_none, &
+                                               line=line, column=column)
+        end if
+    end function parse_import_statement
+
+end module parser_interface_import_module
