31b22522def48ce4651dc8b54a0258d1c63955a3
Christopher Albert <albert@tugraz.at>
2025-11-25 20:33:04 +0100

    fix: address review findings (#2508)

--- examples/f90/issue_2486_class_type_multidecl.f90
+++ examples/f90/issue_2486_class_type_multidecl.f90
@@ -1,12 +1,20 @@
 ! Test for issue #2486: Multi-variable class/type declarations in procedure bodies
 ! This file verifies that multi-variable declarations using class and type keywords
 ! are correctly preserved with all variables.
-subroutine test_class_multidecl()
+module issue_2486_class_type_multidecl_mod
     implicit none
-    class(mytype), allocatable :: a(:), b(:)
-end subroutine test_class_multidecl
 
-subroutine test_type_multidecl()
-    implicit none
-    type(mytype), allocatable :: x, y, z
-end subroutine test_type_multidecl
+    type :: mytype
+        integer :: value
+    end type mytype
+contains
+    subroutine test_class_multidecl()
+        implicit none
+        class(mytype), allocatable :: a(:), b(:)
+    end subroutine test_class_multidecl
+
+    subroutine test_type_multidecl()
+        implicit none
+        type(mytype), allocatable :: x, y, z
+    end subroutine test_type_multidecl
+end module issue_2486_class_type_multidecl_mod
