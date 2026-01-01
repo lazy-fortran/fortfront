bca1884d121b3b112663ce63b41ae4d13344b495
Christopher Albert <albert@tugraz.at>
2026-01-01 19:24:32 +0100

    fix: ERROR-TEST: DATA statement with more values than variables (fixes #2596) (#2684)
    * fix: reject scalar DATA over-initialization (fixes #2596)
    
    * test: assert scalar DATA over-initialization errors

--- examples/f90/data_statement_scalar_upgrade.f90
+++ examples/f90/data_statement_scalar_upgrade.f90
@@ -1,7 +1,7 @@
-! DATA statement should upgrade a scalar declaration to an array
+! ERROR-TEST: DATA statement with more values than variables
 program data_statement_scalar_upgrade
     implicit none
     integer :: values
-    data values /10, 20/
-    print *, values(2)
+    data values/10, 20/
+    print *, values
 end program data_statement_scalar_upgrade
