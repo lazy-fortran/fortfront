e5388c6880ba474eb3b1ce9947026df844dcb2f2
Christopher Albert <albert@tugraz.at>
2026-05-15 20:20:52 +0200

    cleanup: inline codegen_declarations bridge modules (refs #2783)
    codegen_declarations_procedures.f90 and codegen_declarations_programs.f90
    were pure re-export facades used only by codegen_declarations.f90. Drop
    the intermediary modules and have codegen_declarations import directly
    from codegen_function_declarations, codegen_subroutine_declarations,
    codegen_module_generation, and codegen_program_generation.
    
    The forall/if-do parser bridge subroutines stay - they exist to break
    mutual use-dependencies. call_graph_module also stays since it is a
    documented public-API facade re-exported through fortfront.

--- src/codegen/decls/codegen_declarations.f90
+++ src/codegen/decls/codegen_declarations.f90
@@ -1,15 +1,15 @@
 module codegen_declarations
-    use codegen_declarations_procedures, only: generate_code_function_def, &
-                                               generate_code_subroutine_def
+    use codegen_function_declarations, only: generate_code_function_def
+    use codegen_subroutine_declarations, only: generate_code_subroutine_def
     use codegen_declarations_core, only: generate_code_declaration, &
                                          generate_code_parameter_declaration, &
                                          generate_code_derived_type
-    use codegen_declarations_programs, only: generate_code_module, &
-                                             generate_code_submodule, &
-                                             generate_code_interface_block, &
-                                             generate_code_module_procedure, &
-                                             generate_code_program, &
-                                             generate_code_block_data
+    use codegen_module_generation, only: generate_code_module, &
+                                         generate_code_submodule, &
+                                         generate_code_block_data, &
+                                         generate_code_interface_block, &
+                                         generate_code_module_procedure
+    use codegen_program_generation, only: generate_code_program
     implicit none
     private
     public :: generate_code_function_def
