46e3ac52ee961bcdf184e7c38c3d404635b0817b
Christopher Albert <albert@tugraz.at>
2026-05-15 20:27:43 +0200

    cleanup: inline codegen_declarations bridge modules (refs #2783) (#2795)
    codegen_declarations_procedures.f90 and codegen_declarations_programs.f90
    were pure re-export facades used only by codegen_declarations.f90. Drop
    the intermediary modules and have codegen_declarations import directly
    from codegen_function_declarations, codegen_subroutine_declarations,
    codegen_module_generation, and codegen_program_generation.
    
    The forall/if-do parser bridge subroutines stay - they exist to break
    mutual use-dependencies. call_graph_module also stays since it is a
    documented public-API facade re-exported through fortfront.

--- src/codegen/decls/codegen_declarations_procedures.f90
+++
@@ -1,9 +0,0 @@
-module codegen_declarations_procedures
-    use codegen_function_declarations, only: generate_code_function_def
-    use codegen_subroutine_declarations, only: generate_code_subroutine_def
-    implicit none
-    private
-    public :: generate_code_function_def
-    public :: generate_code_subroutine_def
-end module codegen_declarations_procedures
-
