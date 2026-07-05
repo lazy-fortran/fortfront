e43ea899598ef80d8d043c6b2a6867602b07fa60
Christopher Albert <albert@tugraz.at>
2026-07-05 09:04:25 +0200

    fix: benchmark full reparse at LSP scale; document didChange budget (refs #2844) (#2863)
    * feat: add full reparse benchmark for LSP didChange scale (refs #2844)
    
    Benchmark tooling_load_ast_from_string with reuse_arena for 200, 1000,
    5000 line files. Results: 200L ~3ms, 1000L ~40ms, 5000L ~819ms.
    5000 lines exceeds 100ms budget by 8x.
    
    * docs: add LSP-scale reparse benchmarks and didChange budget (refs #2844)
    
    200L ~3ms, 1000L ~40ms, 5000L ~819ms. Full reparse viable up to
    ~800 lines at 100ms budget. Larger files need longer debounce or
    incremental parsing.

---
+++ examples/f90/benchmark_1000_lines.f90
@@ -0,0 +1,1000 @@
+program benchmark
+  implicit none
+  integer :: result
+  integer :: x, y, z
+
+contains
+
+  integer function bench_func_1(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_1 = acc
+  end function bench_func_1
+
+  integer function bench_func_2(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_2 = acc
+  end function bench_func_2
+
+  integer function bench_func_3(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_3 = acc
+  end function bench_func_3
+
+  integer function bench_func_4(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_4 = acc
+  end function bench_func_4
+
+  integer function bench_func_5(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_5 = acc
+  end function bench_func_5
+
+  integer function bench_func_6(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_6 = acc
+  end function bench_func_6
+
+  integer function bench_func_7(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_7 = acc
+  end function bench_func_7
+
+  integer function bench_func_8(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_8 = acc
+  end function bench_func_8
+
+  integer function bench_func_9(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_9 = acc
+  end function bench_func_9
+
+  integer function bench_func_10(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_10 = acc
+  end function bench_func_10
+
+  integer function bench_func_11(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_11 = acc
+  end function bench_func_11
+
+  integer function bench_func_12(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_12 = acc
+  end function bench_func_12
+
+  integer function bench_func_13(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_13 = acc
+  end function bench_func_13
+
+  integer function bench_func_14(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_14 = acc
+  end function bench_func_14
+
+  integer function bench_func_15(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_15 = acc
+  end function bench_func_15
+
+  integer function bench_func_16(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_16 = acc
+  end function bench_func_16
+
+  integer function bench_func_17(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_17 = acc
+  end function bench_func_17
+
+  integer function bench_func_18(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_18 = acc
+  end function bench_func_18
+
+  integer function bench_func_19(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_19 = acc
+  end function bench_func_19
+
+  integer function bench_func_20(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_20 = acc
+  end function bench_func_20
+
+  integer function bench_func_21(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_21 = acc
+  end function bench_func_21
+
+  integer function bench_func_22(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_22 = acc
+  end function bench_func_22
+
+  integer function bench_func_23(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_23 = acc
+  end function bench_func_23
+
+  integer function bench_func_24(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_24 = acc
+  end function bench_func_24
+
+  integer function bench_func_25(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_25 = acc
+  end function bench_func_25
+
+  integer function bench_func_26(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_26 = acc
+  end function bench_func_26
+
+  integer function bench_func_27(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_27 = acc
+  end function bench_func_27
+
+  integer function bench_func_28(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_28 = acc
+  end function bench_func_28
+
+  integer function bench_func_29(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_29 = acc
+  end function bench_func_29
+
+  integer function bench_func_30(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_30 = acc
+  end function bench_func_30
+
+  integer function bench_func_31(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_31 = acc
+  end function bench_func_31
+
+  integer function bench_func_32(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_32 = acc
+  end function bench_func_32
+
+  integer function bench_func_33(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_33 = acc
+  end function bench_func_33
+
+  integer function bench_func_34(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_34 = acc
+  end function bench_func_34
+
+  integer function bench_func_35(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_35 = acc
+  end function bench_func_35
+
+  integer function bench_func_36(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_36 = acc
+  end function bench_func_36
+
+  integer function bench_func_37(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_37 = acc
+  end function bench_func_37
+
+  integer function bench_func_38(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_38 = acc
+  end function bench_func_38
+
+  integer function bench_func_39(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_39 = acc
+  end function bench_func_39
+
+  integer function bench_func_40(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_40 = acc
+  end function bench_func_40
+
+  integer function bench_func_41(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_41 = acc
+  end function bench_func_41
+
+  integer function bench_func_42(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_42 = acc
+  end function bench_func_42
+
+  integer function bench_func_43(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_43 = acc
+  end function bench_func_43
+
+  integer function bench_func_44(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_44 = acc
+  end function bench_func_44
+
+  integer function bench_func_45(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_45 = acc
+  end function bench_func_45
+
+  integer function bench_func_46(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_46 = acc
+  end function bench_func_46
+
+  integer function bench_func_47(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_47 = acc
+  end function bench_func_47
+
+  integer function bench_func_48(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_48 = acc
+  end function bench_func_48
+
+  integer function bench_func_49(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_49 = acc
+  end function bench_func_49
+
+  integer function bench_func_50(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_50 = acc
+  end function bench_func_50
+
+  integer function bench_func_51(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_51 = acc
+  end function bench_func_51
+
+  integer function bench_func_52(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_52 = acc
+  end function bench_func_52
+
+  integer function bench_func_53(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_53 = acc
+  end function bench_func_53
+
+  integer function bench_func_54(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_54 = acc
+  end function bench_func_54
+
+  integer function bench_func_55(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_55 = acc
+  end function bench_func_55
+
+  integer function bench_func_56(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_56 = acc
+  end function bench_func_56
+
+  integer function bench_func_57(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_57 = acc
+  end function bench_func_57
+
+  integer function bench_func_58(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_58 = acc
+  end function bench_func_58
+
+  integer function bench_func_59(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_59 = acc
+  end function bench_func_59
+
+  integer function bench_func_60(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_60 = acc
+  end function bench_func_60
+
+  integer function bench_func_61(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_61 = acc
+  end function bench_func_61
+
+  integer function bench_func_62(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_62 = acc
+  end function bench_func_62
+
+  integer function bench_func_63(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_63 = acc
+  end function bench_func_63
+
+  integer function bench_func_64(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_64 = acc
+  end function bench_func_64
+
+  integer function bench_func_65(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_65 = acc
+  end function bench_func_65
+
+  integer function bench_func_66(a, b)
+    integer, intent(in) :: a, b
+    integer :: temp
+    integer :: acc
+    integer :: k
+
+    acc = 0
+    temp = a + b
+    do k = 1, 10
+      acc = acc + temp * k
+    end do
+    bench_func_66 = acc
+  end function bench_func_66
+
+
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+  ! padding line
+end program benchmark
